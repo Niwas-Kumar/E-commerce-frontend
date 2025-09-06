99c32d06f5b535aec5f8f0c44c6b1d6cfda79505
Niwas Kumar <niwastechster@gmail.com>
2025-09-06 18:28:04 +0530

    .

--- src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// Replace this with your deployed backend URL
-const API = axios.create({
-  baseURL: "https://e-commerce-frontend-bice-phi.vercel.app/api",
-});
-
-export default API;
